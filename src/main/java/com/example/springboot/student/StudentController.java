e2b2f9cd6ea3f2e59bf42920e33fc829aa5247ad
collins <collinsboniface1000@gmail.com>
2021-10-06 18:25:32 +0300

    The update to the application.properties file worked and the Hibernate work at the start of the server

--- src/main/java/com/example/springboot/student/StudentController.java
+++ src/main/java/com/example/springboot/student/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
