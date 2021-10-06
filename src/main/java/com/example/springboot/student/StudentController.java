cd96e2e05a9a502ebd2ce82cd62f86f918094df9
collins <collinsboniface1000@gmail.com>
2021-10-06 15:40:05 +0300

    The update to the Student Controller and Student Service class Worked

--- src/main/java/com/example/springboot/student/StudentController.java
+++ src/main/java/com/example/springboot/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.springboot.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,20 +13,17 @@ import java.util.List;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
 
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = new StudentService();
+    }
 
 
     @GetMapping
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Collin",
-                        "collinsboniface@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                )
-        );
+        return studentService.getStudents();
     }
 
 
--- src/main/java/com/example/springboot/student/StudentService.java
+++ src/main/java/com/example/springboot/student/StudentService.java
@@ -1,4 +1,26 @@
 package com.example.springboot.student;
 
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+//It has to be instantiated, and Also it is a bean
+//What is the difference between the two annotation @Component and @Service
+
+@Service
 public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Collin",
+                        "collinsboniface@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+        );
+    }
 }
