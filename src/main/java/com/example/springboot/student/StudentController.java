0c55e4f639fc60b360dfe372f3d5583f743d2f59
collins <collinsboniface1000@gmail.com>
2021-10-06 15:05:31 +0300

    Making the controller class and changing the path of the JSON

--- src/main/java/com/example/springboot/student/StudentController.java
+++ src/main/java/com/example/springboot/student/StudentController.java
@@ -1,10 +1,32 @@
 package com.example.springboot.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
+
+
+
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Collin",
+                        "collinsboniface@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+        );
+    }
+
 
 }
