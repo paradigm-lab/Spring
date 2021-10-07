fb6f8c1037d105c9a3c6f9e525cf922017e2da24
collins <collinsboniface1000@gmail.com>
2021-10-07 06:32:20 +0300

    Making the Student configuration class that takes the name to the datebase BUT it takes only one student to the database -> NOT working as ecspted

--- src/main/java/com/example/springboot/student/StudentConfig.java
+++ src/main/java/com/example/springboot/student/StudentConfig.java
@@ -1,8 +1,48 @@
 package com.example.springboot.student;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+//What is a static import
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
 
 @Configuration
 public class StudentConfig {
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student alex = new Student(
+                    1L,
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2003, JANUARY, 17),
+                    21
+            );
+
+            Student matt = new Student(
+                    1L,
+                    "Matt",
+                    "Matt@gmail.com",
+                    LocalDate.of(1980, JANUARY, 15),
+                    30
+            );
+
+            Student collin = new Student(
+                    1L,
+                    "Collin",
+                    "collinsboniface1000@gmail.com",
+                    LocalDate.of(2000, JANUARY, 8),
+                    49
+            );
+            repository.saveAll(
+                    List.of(collin, alex, matt)
+            );
+        };
+    }
 }
